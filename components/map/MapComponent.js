1bb69c6b04a9b2f3df3998e1ad5632e9fdc05a63
axolotlbyte <arjaankhan@gmail.com>
2025-08-17 10:23:37 +0300

    added markers to the map

--- components/map/MapComponent.js
+++ components/map/MapComponent.js
@@ -7,12 +7,37 @@ import {
   Popup,
   ImageOverlay,
 } from "react-leaflet";
+import L from "leaflet";
 
-const imageUrl = "/images/map/map.jpg"; // Replace with your image path
+const imageUrl = "/images/map/map.jpg";
 const imageBounds = [
   [0, 0],
   [9150, 11275],
-]; // [height, width]
+];
+
+const AmenitiesIcon = L.divIcon({
+  className: "custom-label label-text-map",
+  html: "<div>Amenities</div>",
+  iconSize: [80, 40],
+});
+
+const SecurityIcon = L.divIcon({
+  className: "custom-label label-text-map",
+  html: "<div>Security Main Gate</div>",
+  iconSize: [140, 40],
+});
+
+const AdminIcon = L.divIcon({
+  className: "custom-label label-text-map",
+  html: "<div>Admin Building</div>",
+  iconSize: [100, 40],
+});
+
+const ZoneNineIcon = L.divIcon({
+  className: "custom-label label-text-map",
+  html: "<div>Zone 9 Pool</div>",
+  iconSize: [80, 40],
+});
 
 export default function MapComponent() {
   return (
@@ -27,8 +52,13 @@ export default function MapComponent() {
       touchZoom={true}
       doubleClickZoom={true}
       dragging={true}
+      attributionControl={false}
     >
       <ImageOverlay url={imageUrl} bounds={imageBounds} />
+      <Marker position={[4800, 6200]} icon={AmenitiesIcon} />
+      <Marker position={[2000, 7850]} icon={SecurityIcon} />
+      <Marker position={[3300, 9850]} icon={AdminIcon} />
+      <Marker position={[4600, 4200]} icon={ZoneNineIcon} />
     </MapContainer>
   );
 }
